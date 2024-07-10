15fc3c63716e68103f9edd7a92092e162bee0eab
richiparada <ricardo.parada.espinoza@gmail.com>
2024-07-10 17:18:57 -0400

    Modificación
    Se agrega archivo urls.py en aplicación proveedores, y se modifica el archivo urls.py de la aplicación django para incluir la ruta del archivo urls.py de proveedores.

--- sicoap/urls.py
+++ sicoap/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('proveedores.urls')) #llama a las URL de la app proveedores y la utiliza sin un prefijo anterior
 ]
