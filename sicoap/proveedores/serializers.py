f6e78fdc68bb0d2b0b4e13234aecb2144d532716
richiparada <ricardo.parada.espinoza@gmail.com>
2024-07-10 01:10:53 -0400

    Reinicio de la infraestructura
    Se reinicia la infraestructura producto de problemas entre backend y fontend, buscando realizar paso a paso las actividades.

--- sicoap/proveedores/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Proveedor
-
-class ProveedorSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Proveedor
-        fields = ['id', 'nombre', 'empresa', 'numero_contacto']
-        
