e511f24b9727f3dd48093e0ac2c4d65875212291
richiparada <ricardo.parada.espinoza@gmail.com>
2024-07-22 13:24:26 -0400

    Ventana de administración
    Se agrega a las ventanas de administración, las ventanas de proveedor y bodega

--- proveedores/admin.py
+++ proveedores/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Proveedor, Bodega
 
-# Register your models here.
+@admin.register(Proveedor)
+class ProveedorAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'rut', 'placa_patente', 'empresa', 'numero_contacto')
+    search_fields = ('nombre', 'rut', 'empresa', 'placa_patente')
+
+@admin.register(Bodega)
+class BodegaAdmin(admin.ModelAdmin):
+    list_display = ('tipo_bodega', 'nombre_responsable', 'proveedor')
+    search_fields = ('tipo_bodega', 'nombre_responsable', 'proveedor__nombre')
+    list_filter = ('proveedor',)
