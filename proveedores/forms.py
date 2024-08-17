3f8ffd9676cabb619c890d9bac382243d980e5f6
richiparada <ricardo.parada.espinoza@gmail.com>
2024-08-17 11:13:33 -0400

    Prueba
    Prueba

---
+++ proveedores/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import Proveedor
+
+class ProveedorForm(forms.ModelForm):
+    class Meta:
+        model = Proveedor
+        fields = ['nombre', 'rut', 'empresa', 'placa_patente', 'numero_contacto']
+        widgets = {
+            'nombre': forms.TextInput(attrs={'class': 'nombre', 'placeholder': 'Escriba Nombre del proveedor'}),
+            'rut': forms.TextInput(attrs={'class': 'rut', 'placeholder': 'Escriba Rut del proveedor'}),
+            'empresa': forms.TextInput(attrs={'class': 'empresa', 'placeholder': 'Ingrese nombre de la Empresa'}),
+            'placa_patente': forms.TextInput(attrs={'class': 'placa_patente', 'placeholder': 'Ingrese Placa Patente del Vehículo'}),
+            'numero_contacto': forms.TextInput(attrs={'class': 'numero_contacto', 'placeholder': 'Ingrese número de contacto'}),
+        }
--- proveedores/views.py
+++ proveedores/views.py
@@ -1,7 +1,8 @@
 from rest_framework import viewsets
 from .models import Proveedor, Bodega
 from .serializers import ProveedorSerializer, BodegaSerializer
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ProveedorForm
 
 class ProveedorViewSet(viewsets.ModelViewSet):
     queryset = Proveedor.objects.all()
@@ -15,10 +16,24 @@ def proveedores_list(request):
     proveedores = Proveedor.objects.all()
     return render(request, 'index.html', {'proveedores': proveedores})
 
-def proveedores_registro(request):
+'''def proveedores_registro(request):
     proveedores = Proveedor.objects.all()
-    return render(request, 'registro.html', {'proveedores': proveedores})
+    return render(request, 'registro.html', {'proveedores': proveedores})'''
 
 def proveedores_info(request):
     proveedores = Proveedor.objects.all()
     return render(request, 'info.html', {'proveedores': proveedores})
+
+def proveedores_registro(request):
+    if request.method == "POST":
+        form = ProveedorForm(request.POST)
+        print(form)
+        if form.is_valid():
+            form.save()
+            print(form)
+            return redirect('proveedores_list')  # Redirige a la lista de proveedores después de guardar   
+
+    else:
+        form = ProveedorForm()
+    
+    return render(request, 'registro.html', {'form': form})
