d7f1f3b2a864df0961a162888552325a1d27cba4
richiparada <ricardo.parada.espinoza@gmail.com>
2024-08-28 01:35:17 -0400

    Normalización de datos
    Se agrega función en el formulario que normaliza los datos ingresados por el usuario.

--- proveedores/forms.py
+++ proveedores/forms.py
@@ -13,3 +13,31 @@ class ProveedorForm(forms.ModelForm):
             'placa_patente': forms.TextInput(attrs={'class': 'placa_patente', 'placeholder': 'Ingrese Placa Patente del Vehículo'}),
             'numero_contacto': forms.TextInput(attrs={'class': 'numero_contacto', 'placeholder': 'Ingrese número de contacto'}),
         }
+    
+    def clean_nombre(self):
+        nombre = self.cleaned_data.get('nombre')
+        return nombre.strip().title() # Eliminar espacios y capitalizar cada palabra
+    
+    def clean_apellido(self):
+        apellido = self.cleaned_data.get('apellido')
+        return apellido.strip().title() # Eliminar espacios y capitalizar cada palabra
+    
+    def clean_empresa(self):
+        empresa = self.cleaned_data.get('empresa')
+        return empresa.strip().upper()  # Eliminar espacios y convertir a mayúsculas
+    
+    def clean_rut(self):
+        rut = self.cleaned_data.get('rut')
+        return rut.replace('.', '').replace('-', '').upper()  # Eliminar puntos, guiones y convertir a mayúsculas
+
+    def clean_placa_patente(self):
+        placa_patente = self.cleaned_data.get('placa_patente')
+        return placa_patente.replace('-', '').upper()  # Eliminar guiones y convertir a mayúsculas
+
+    def clean_numero_contacto(self):
+        numero_contacto = self.cleaned_data.get('numero_contacto')
+        return ''.join(filter(str.isdigit, numero_contacto))  # Mantener solo dígitos
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        return email.strip().lower()  # Eliminar espacios y convertir a minúsculas
