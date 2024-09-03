2c830369b1169872add0c6e51319c18ea5e26c7a
richiparada <ricardo.parada.espinoza@gmail.com>
2024-09-03 18:51:19 -0400

    Se modifica template
    Se agrega opción de bodegas para ver en template de información.

--- proveedores/admin.py
+++ proveedores/admin.py
@@ -3,11 +3,11 @@ from .models import Proveedor, Bodega
 
 @admin.register(Proveedor)
 class ProveedorAdmin(admin.ModelAdmin):
-    list_display = ('nombre', 'apellido', 'rut', 'placa_patente', 'empresa', 'numero_contacto')
-    search_fields = ('nombre', 'apellido', 'rut', 'empresa', 'placa_patente')
+    list_display = ('nombre', 'apellido', 'rut', 'placa_patente', 'empresa', 'numero_contacto', 'bodega')
+    search_fields = ('nombre', 'apellido', 'rut', 'empresa', 'placa_patente', 'numero_contacto', 'bodega')
 
 @admin.register(Bodega)
 class BodegaAdmin(admin.ModelAdmin):
-    list_display = ('tipo_bodega', 'nombre_responsable', 'proveedor')
-    search_fields = ('tipo_bodega', 'nombre_responsable', 'proveedor__nombre')
+    list_display = ('tipo_bodega', 'nombre_responsable')
+    search_fields = ('tipo_bodega', 'nombre_responsable')
     list_filter = ('proveedor',)
