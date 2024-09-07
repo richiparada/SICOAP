e39f5bbf7be4e5f0971547925030fd9b761846e0
richiparada <ricardo.parada.espinoza@gmail.com>
2024-09-06 20:55:05 -0400

    Cerrar Sesión
    Se agrega botón de cerrar sesión

--- proveedores/urls.py
+++ proveedores/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, include
+from django.contrib.auth.views import LogoutView
 from .views import proveedores_list, proveedores_registro, proveedores_info, marcar_retiro, estadisticas_proveedores, bodega_list, bodega_crear, estadisticas_bodegas, registro, iniciar_sesion
 
 
@@ -16,4 +17,5 @@ urlpatterns = [
     path('estadisticas_bodega/', estadisticas_bodegas, name='estadisticas_bodegas'),
     path('registro_usuarios/', registro, name='registro'),
     path('login/', iniciar_sesion, name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
